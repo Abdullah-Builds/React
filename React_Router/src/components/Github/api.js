99ac2caea346149e7ea12d3560e4af171698db83
Muhammad Abdullah khan <khan.abdullah135790@gmail.com>
2025-05-16 10:53:59 -0700

    Remove all files

--- React_Router/src/components/Github/api.js
+++
@@ -1,4 +0,0 @@
-export async function Github_Api(){
-    const response = await fetch('https://api.github.com/users/Abdullah-Builds')
-    return response.json()
- }
--- React_Router/src/components/User/User.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { useParams } from "react-router";
-
-export default function User(){
-    const {userid} = useParams() 
-    return(
-        <div className="text-center bg-amber-50 p-4 m-4" >User : {userid}</div>
-    )
-}
